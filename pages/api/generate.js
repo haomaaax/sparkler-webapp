6c03239c916b8be20144dab9a20ff8543622963b
maaax.eth <maxyay5566@gmail.com>
2022-12-21 18:57:24 +0800

    init with gpt-3 project with prompts

---
+++ pages/api/generate.js
@@ -0,0 +1,33 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const basePromptPrefix =
+`
+Generate a storyline that includes the following elements or characters with vivid description,   
+
+Headline:
+
+Storyline:
+`
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}\n`,
+    temperature: 0.7,
+    max_tokens: 250,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
---
+++ pages/index.js
@@ -0,0 +1,114 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import buildspaceLogo from '../assets/buildspace-logo.png';
+import twitterLogo from '../assets/twitter-logo.png';
+import { useState } from 'react';
+
+const Home = () => {
+  const [userInput, setUserInput] = useState('');
+
+  const [apiOutput, setApiOutput] = useState('')
+const [isGenerating, setIsGenerating] = useState(false)
+
+const callGenerateEndpoint = async () => {
+  setIsGenerating(true);
+  
+  console.log("Calling OpenAI...")
+  const response = await fetch('/api/generate', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ userInput }),
+  });
+
+  const data = await response.json();
+  const { output } = data;
+  console.log("OpenAI replied...", output.text)
+
+  setApiOutput(`${output.text}`);
+  setIsGenerating(false);
+}
+
+  const onUserChangedText = (event) => {
+    console.log(event.target.value);
+    setUserInput(event.target.value);
+  };
+
+  return (
+    <div className="root">
+      <Head>
+        <title>Sparkler 🎇</title>
+      </Head>
+      <div className="container">
+        <div className="header">
+          <div className="header-title">
+            <h1>Create your own comic</h1>
+          </div>
+          <div className="header-subtitle">
+            <h2>Enter anything you want to show up in storyline</h2>
+          </div>
+        </div>
+        {/* Add prompt text box here */}
+        <div className="prompt-container">
+          <textarea
+            className="prompt-box"
+            placeholder="Something like a hero with legendary sword or mysterious dragons in dark dungeon"
+            value={userInput}
+            onChange={onUserChangedText}
+          />;
+          {/* Add button here */}
+          <div className="prompt-buttons">
+            <a
+              className={isGenerating ? 'generate-button loading' : 'generate-button'}
+              onClick={callGenerateEndpoint}
+            >
+              <div className="generate">
+                {isGenerating ? <span className="loader"></span> : <p>Generate</p>}
+              </div>
+            </a>
+          </div>
+          {/* New code I added here */}
+          {apiOutput && (
+            <div className="output">
+              <div className="output-header-container">
+                <div className="output-header">
+                  <h3>Storyline based on your prompt</h3>
+                </div>
+              </div>
+              <div className="output-content">
+                <p>{apiOutput}</p>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+      <div className="badge-container grow">
+        <a
+          href="https://buildspace.so/builds/ai-writer"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className="badge">
+            <Image src={buildspaceLogo} alt="buildspace logo" />
+            <p>build with buildspace</p>
+          </div>
+        </a>
+      </div>
+      <div className="badge-container-right grow">
+        <a
+          href="https://twitter.com/haomaaax"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className="badge">
+            <Image src={twitterLogo} alt="twitter logo" />
+            <p>built by maaax</p>
+          </div>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
