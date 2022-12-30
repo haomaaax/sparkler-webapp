2630f95a753c9fe912df9c5d184b47d9cf94c28d
maaax.eth <maxyay5566@gmail.com>
2022-12-31 01:07:42 +0800

    fix: open tweet out link in new tab

--- components/shareTweet.jsx
+++ components/shareTweet.jsx
@@ -1,7 +1,11 @@
 const ShareTweet = () => {
   return (
     <div className="twitter-share">
-      <a href="https://twitter.com/intent/tweet?url=https%3A%2F%2Fsparkler.club%2F&text=Create+my+FIRST+comic+with+AI+🤯+Try+it+here+🎇:&via=haomaaax&hashtags=GPT3%2CDALLE2%2CAI">
+      <a
+        href="https://twitter.com/intent/tweet?url=https%3A%2F%2Fsparkler.club%2F&text=Create+my+FIRST+comic+with+AI+🤯+Try+it+here+🎇:&via=haomaaax&hashtags=GPT3%2CDALLE2%2CAI"
+        target="_blank"
+        rel="noreferrer noopener"
+      >
         <p>Tweet out your spark 🎇</p>
       </a>
     </div>
